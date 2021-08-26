39be88008128ebc2b1176a60a25338e6f4cd7ac7
Wenping Guo <ybyygu@gmail.com>
2021-08-26 11:07:06 +0800

    Remove diesel related codes

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ extern crate diesel;
 // imports:1 ends here
 
 // [[file:../zotero.note::*mods][mods:1]]
-pub mod models;
-pub mod schema;
+// pub mod models;
+// pub mod schema;
+// mod database;
 
-mod database;
 mod db;
 mod profile;
 mod server;
@@ -18,23 +18,23 @@ mod server;
 // [[file:../zotero.note::*pub][pub:1]]
 use gut::prelude::*;
 
-/// Return PDF attachment path from zotero protocol link
-///
-/// # Parameters
-/// ----------
-/// * link: zotero item selection link, e.g: zotero://select/items/1_NIUYMGLJ
-pub fn get_attachment_from_link(link: &str) -> Result<Option<String>> {
-    use crate::database::*;
-    use crate::server::*;
+// /// Return PDF attachment path from zotero protocol link
+// ///
+// /// # Parameters
+// /// ----------
+// /// * link: zotero item selection link, e.g: zotero://select/items/1_NIUYMGLJ
+// pub fn get_attachment_from_link(link: &str) -> Result<Option<String>> {
+//     use crate::database::*;
+//     use crate::server::*;
 
-    let url = "/home/ybyygu/Data/zotero/zotero.sqlite.bak";
-    let zot_server = ZoteroServer::default();
-    let zot_db = ZoteroDb::connect(url)?;
+//     let url = "/home/ybyygu/Data/zotero/zotero.sqlite.bak";
+//     let zot_server = ZoteroServer::default();
+//     let zot_db = ZoteroDb::connect(url)?;
 
-    zot_server
-        .get_attachment(link)
-        .or(zot_db.get_attachment_from_link(link))
-}
+//     zot_server
+//         .get_attachment(link)
+//         .or(zot_db.get_attachment_from_link(link))
+// }
 
 /// Create a new `report` item in zotero with a .note (org-mode) attachment, and
 /// returns zotero uri of the new item.
@@ -50,11 +50,14 @@ pub use crate::db::{get_item_key_from_link, get_items_by_tag, Item};
 
 // [[file:../zotero.note::*test][test:1]]
 #[test]
-fn test_get_attachment() {
+fn test_get_attachment() -> Result<()> {
     let link = "zotero://select/items/1_U5MRLMBI";
-    let attachment = get_attachment_from_link(link).expect("zotero attach");
-    assert!(attachment.is_some());
-    let path = std::path::PathBuf::from(attachment.unwrap());
+    let key = get_item_key_from_link(link)?;
+    let item: Item = key.parse()?;
+    let attachments = item.attachment_paths();
+    assert_eq!(attachments.len(), 1);
+    let path = std::path::PathBuf::from(&attachments[0]);
     assert!(path.exists());
+    Ok(())
 }
 // test:1 ends here
